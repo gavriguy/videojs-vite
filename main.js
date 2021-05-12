113abd07e0ee8a8c450621dff1a2497418459aa3
gavriguy <gavri.guy@gmail.com>
2021-05-12 21:32:20 +0300

    connect to feed

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-import './style.css'
 import videojs from 'video.js'
+import * as axios from 'axios'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
 
 var options = {};
 
-var player = videojs('my-player', options, function onPlayerReady() {
-  videojs.log('Your player is ready!');
 
-  // In this context, `this` is the player that was created by Video.js.
-  // this.play();
+videojs('my-player', options, function onPlayerReady() {
 
-  // How about an event listener?
-  this.on('ended', function () {
-    videojs.log('Awww...over so soon?!');
-  });
+
+  axios.get('https://zapp-pipes2-demo.web.app/jw/playlists/q87jSmY5').then(({ data }) => {
+    const src = data.entry[1].content.src
+
+    this.src({
+      src: data.entry[1].content.src,
+      type: 'application/x-mpegURL'
+    });
+
+  })
+
+
 });
